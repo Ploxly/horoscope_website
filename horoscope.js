c0f9510fd19044e96da0372bb418fa36593bd36b
Shane Whiteman <shanewhiteman@shanes-mbp.lan>
2019-12-28 12:08:33 -0500

    functionality added so Aries can generate a horoscope

--- horoscope.js
+++ horoscope.js
@@ -1,17 +1,41 @@
-//detect that the selector has change and figure out which value it has changed to
-//Once I figure out the value check if value is aries 
-// if aries replace some text on screen with a horoscope
+//detect that the selector has change and figure out which value it has changed to ✔
+//Once I figure out the value check if value is aries ✔
+// if aries replace some text on screen with a horoscope ✔
+// at least 4 different horoscope types
+//both horoscope and gender has to be modified from their default values before a horoscope shows
 
 function generateHoroscope() {
-    let horoscope_select = document.getElementById("horoscope_select");
-    let select_value = horoscope_select.options[horoscope_select.selectedIndex].value;
+    let zodiac_select = document.getElementById("zodiac_select");
+    //let gender_select = document.getElementById("gender_select");
+    let select_value_zodiac = zodiac_select.options[zodiac_select.selectedIndex].value;
+    //let select_value_gender = gender_select.options[gender_select.selectedIndex].value;
 
-    console.log(select_value)
-    return;
+
+    let welcome_msg = document.getElementById("welcome_message");
+
+    if (select_value_zodiac == "Aries") {
+
+        welcome_msg.innerText = "Well done!"
+       // change_welcome_msg.innerHTML = "" .replace(/Welcome! Please choose your zodiac sign and gender to begin.../g, "Well done!")
+        return;
+        
+    }
+    else {
+        return welcome_msg.innerText = "Welcome! Please choose your zodiac sign and gender to begin...";
+    }
+
+    //console.log(select_value_zodiac, select_value_gender)
+    //return;
 }
 
 function main() {
     document.addEventListener("DOMContentLoaded",function(){
-        generateHoroscope()
+        let zodiac_select = document.getElementById("zodiac_select");
+
+        zodiac_select.addEventListener("change", function(){
+            generateHoroscope()
+        })
     });
 }
+
+main()
